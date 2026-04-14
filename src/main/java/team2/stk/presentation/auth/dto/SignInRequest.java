d8d71d6b8c9bc97e2aba4a400c0a2b09b36ac43c
hsm <hll2071@naver.com>
2026-04-14 12:13:31 +0900

    refactor : 이메일 검증 임시 비활성화

--- src/main/java/team2/stk/presentation/auth/dto/SignInRequest.java
+++ src/main/java/team2/stk/presentation/auth/dto/SignInRequest.java
@@ -12,7 +12,6 @@ public class SignInRequest {
 
     @NotBlank(message = "이메일은 필수입니다.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
-    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@stk-eng\\.com$", message = "사내 이메일만 사용할 수 있습니다.")
     private String email;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
